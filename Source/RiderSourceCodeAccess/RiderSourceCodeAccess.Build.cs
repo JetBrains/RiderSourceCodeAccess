1b2f627aa8c94768bb6b2fc3d94da7d047eec491
hellg1 <56997239+hellg1@users.noreply.github.com>
2022-11-04 12:42:28 +0300

    Fix dependency on EditorFramework module for UE4.27 and older (#27)

--- Source/RiderSourceCodeAccess/RiderSourceCodeAccess.Build.cs
+++ Source/RiderSourceCodeAccess/RiderSourceCodeAccess.Build.cs
@@ -22,7 +22,9 @@ namespace UnrealBuildTool.Rules
 
 			if (Target.Type == TargetType.Editor)
 			{
+				#if UE_5_0_OR_LATER
 				PrivateDependencyModuleNames.Add("EditorFramework");
+				#endif
 				PrivateDependencyModuleNames.Add("UnrealEd");
 				PrivateDependencyModuleNames.Add("GameProjectGeneration");
 			}
